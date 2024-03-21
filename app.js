53b1962fdd4a55522b5167fc7011710a280261d0
Shelby Godding <s539355@nwmissouri.edu>
2024-03-20 22:59:24 -0500

    new endpoint

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var myDataRouter = require('./routes/mydata');
+var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', mydataRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
